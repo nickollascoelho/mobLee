7db3a70808160dc13037ed76ee364fe5c7b6940a
Nickollas Coelho <nickollas.coelho@gmail.com>
2015-07-26 01:51:06 -0300

    Added typeahead.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,41 @@
+var properties = ['is_answered',	'score',	'creation_date',	'question_id',	'link',	'title',	'owner_name'];
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
+
+  $('.typeahead').typeahead(
+    {
+      hint: true,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      name: 'properties',
+      source: substringMatcher(properties)
+    }
+  );
 
   var myModal = $('#myModal');
 

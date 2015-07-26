09c192dcaee8f47ed3fe405c8b62b2b48330cc60
Nickollas Coelho <nickollas.coelho@gmail.com>
2015-07-26 00:31:32 -0300

    Added stackoverlow api call and questions persistence

--- server/routes.js
+++ server/routes.js
@@ -6,9 +6,21 @@
 
 var Result = require('./models/result'),
     Question = require('./models/question'),
-    path = require('path');
+    path = require('path'),
+    request = require('request'),
+    zlib = require('zlib');
 
 module.exports = function(app) {
+
+  app.get('/v1/persist', function(request, response) {
+    requestWithEncoding(options, function(err, data) {
+      if (err) response.send(err);
+      else addQuestions(data, function(err) {
+        if (err) response.send(err);
+        else response.send('Success');
+      });
+    });
+  });
 
   app.get('/:folder/:filename', function(req, res) {
     var filename = req.params.filename;
@@ -20,6 +32,53 @@ module.exports = function(app) {
 
   app.get('*', function(request, response) {
     response.sendFile(path.resolve('client/index.html'));
+  });
+};
+
+var options = {
+  url: 'http://api.stackexchange.com/2.2/search?page=1&pagesize=99&order=desc&sort=creation&tagged=php&site=stackoverflow',
+  headers: {
+    'accept-charset' : 'ISO-8859-1,utf-8;',
+    'accept-encoding' : 'gzip,deflate'
+  }
+};
+
+var requestWithEncoding = function(options, callback) {
+  var req = request.get(options);
+
+  req.on('response', function(res) {
+    var chunks = [];
+    res.on('data', function(chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on('end', function() {
+      var buffer = Buffer.concat(chunks);
+      var encoding = res.headers['content-encoding'];
+      if (encoding === 'gzip') {
+        zlib.gunzip(buffer, function(err, decoded) {
+          callback(err, decoded && decoded.toString());
+        });
+      } else {
+        callback(null, buffer.toString());
+      }
+    });
+  });
+
+  req.on('error', function(err) {
+    callback(err);
+  });
+};
+
+var addQuestions = function(data, callback) {
+  var questions = JSON.parse(data).items;
+  Question.create(questions, function(err, docs) {
+    Result.find({}, function(err, docs) {
+      if (err) callback(err);
+      else Result.update(docs[0], function(err, doc) {
+            callback(err);
+           });
+    });
   });
 };
 
